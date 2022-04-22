fa7d697144db08bfbe58c4f173ac7b12480a94f0
Ajelandro19 <almon190699@gmail.com>
2022-04-22 18:52:32 -0500

    Paso 4: Agregar requerimiento sobre el uso de un getter

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -9,4 +9,10 @@ describe("Unit Test for Spiderman Class", () => {
       expect(andrewgarfield.movies).toBe(2);
       expect(andrewgarfield.studio).toBe('Sony');
     });
+    test('2) Use the method getInfo()', () => {
+        const tomHolland = new Spiderman('Spiderman Sony',25,'Tom Holland',5, 'Marvel')
+  
+        expect(tomHolland.getInfo).toBe("Hey, I'm Tom Holland from Marvel studio");
+       
+      });
   })
