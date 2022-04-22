f1fd0c5a428fbf13cac5ca9587e4021b294290ee
Ajelandro19 <almon190699@gmail.com>
2022-04-22 18:42:07 -0500

    Paso 2: Establecer requerimientos en la prueba

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,7 +1,12 @@
 
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(18);
+describe("Unit Test for Spiderman Class", () => {
+    test('1) Create an spiderman object', () => {
+      const andrewgarfield = new Spiderman('Spiderman Sony',31,'Andrew Garfield',2, 'Sony')
+
+      expect(andrewgarfield.name).toBe('Spiderman Sony');
+      expect(andrewgarfield.age).toBe(31);
+      expect(andrewgarfield.actor).toBe('Andrew Garfield');
+      expect(andrewgarfield.movies).toBe(2);
+      expect(andrewgarfield.studio).toBe('Sony');
     });
   })
